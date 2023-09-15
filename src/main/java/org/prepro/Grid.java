94cfc135201170c5f6e272e440d17b7214e474b7
ned <baptiste.denis@etu.univ-poitiers.fr>
2023-09-15 15:05:40 +0200

    Add structure for Grid class

--- src/main/java/org/prepro/Grid.java
+++ src/main/java/org/prepro/Grid.java
@@ -1,5 +1,14 @@
 package org.prepro;
 
 public class Grid {
-    
+    private Box[][] board;
+    private int xSize, ySize;
+
+    public Grid() {
+        board = new Box[9][9];
+    }
+
+    public void print() {
+    }
+
 }
