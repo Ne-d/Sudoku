499993a01be876af8020057baa24cee0f7828a65
nageur2 <nathamhy@gmail.com>
2023-09-16 10:31:15 +0200

    ajout de la fonction génerate et correction mineur

--- src/main/java/org/prepro/Grid.java
+++ src/main/java/org/prepro/Grid.java
@@ -2,18 +2,40 @@ package org.prepro;
 
 public class Grid {
     private Box[][] board;
-    private final int XSIZE, YSIZE;
+    private final int XSIZE, YSIZE;     // nombrede case XSIZE: ligne , YSIZE: colonne
+    private final int DIFFICULTY;       // nombre de case affecter au début 
 
+// génère une grille avec comme taille 9*9 et 19 case inisializé au départ    
     public Grid() {
-        board = new Box[9][9];
-        XSIZE = 9;
-        YSIZE = 9;
+        this.board = new Box[9][9];
+        this.XSIZE = 9;
+        this.YSIZE = 9;
+        this.DIFFICULTY = 19;
         for(int x = 0; x < XSIZE; x++) {
             for(int y = 0; y < YSIZE; y++) {
-                board[x][y] = new Box();
+                this.board[x][y] = new Box();
             }
         }
+        this.generate_number();
     }
+    // ajoute DIFFICULTY nombre sur la grille de départ
+    private void generate_number(){
+        int pos_x,pos_y;
+        boolean taken;
+        for(int i = 0; i< this.DIFFICULTY; i++ ){
+            do{
+            taken = true;
+            System.out.print("k");
+            pos_x = (int)(Math.random() * (this.XSIZE));
+            pos_y = (int)(Math.random() * (this.YSIZE));
+            if (this.board[pos_x][pos_y].getVal() == 0){
+            this.board[pos_x][pos_y].setVal( (int)(Math.random() * (Math.max(this.XSIZE,this.YSIZE) +1 )) );
+            taken = false;
+            System.out.print(" " + this.board[pos_x][pos_y].getVal() +"(" + pos_x + "," + pos_y +")");
+            }
+            }while(valid_val(pos_x, pos_y) && !taken);
+        }    
+    } 
 
     public void print() {
         for(int x = 0; x < XSIZE; x++) {
@@ -39,7 +61,7 @@ public class Grid {
         return true;
     }
 
-    public boolean valid_val( int column, int line){
+    public boolean valid_val( int line, int column){
         int val = this.board[column][line].getVal();
         for(int i =0; i< 9; i++){
             if(this.board[i][column].getVal() == val && this.board[line][i].getVal() != 0){return false;}
